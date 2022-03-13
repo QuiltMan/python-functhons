ffa959435f333c4f96c900d372288596738b5c0a
Pengcheng Chen <34759341+QuiltMan@users.noreply.github.com>
2022-03-13 11:12:01 +0800

    Update tptran.py

--- tptran.py
+++ tptran.py
@@ -88,12 +88,12 @@ def tptran(wave,t, xoff, pmin, pmax, dp, p_num):
     
     spec2, f, trace_num, sample_num = fftrl2(wave_padding,tau)
     
-    stp=np.zeros((p_num,nt2),dtype = complex)
+    stp=np.zeros((p_num,nt2))
     
     for loop_p in range(p_num):
         dtx=p[loop_p]*xoff
 
-        shiftr=(np.zeros((len(f),nx),dtype = complex)+(1j))*2.0*np.pi
+        shiftr=(np.zeros((len(f),len(xoff)),dtype = complex)+(1j))*2.0*np.pi
 
         for l in range(len(f)):
             for m in range(len(dtx)):
@@ -108,4 +108,39 @@ def tptran(wave,t, xoff, pmin, pmax, dp, p_num):
     stp=stp/p_num
 
     
-    return stp, tau
+    return stp, tau, p
+
+
+def itptran(stp,tau, p, xmin, xmax, dx):
+    tptrace_num = len(stp)
+    tpsample_num = len(stp[0])
+
+    tpnt=int(2**(np.ceil(np.log2(tpsample_num))))
+    if tpsample_num<tpnt:
+        padding=np.zeros((tpnt-tpsample_num,tptrace_num),dtype = complex)
+        stp_padding=np.c_[stp,padding.T]
+        it = (tau[1]-tau[0])*(np.linspace(0, tpnt-1, tpnt))
+    else:
+        stp_padding=stp
+        it=tau
+
+    stpf, f, _, _=fftrl2(stp_padding,it)    
+    ix_num=int((xmax-xmin)/dx)+1
+    ix = np.linspace(xmin, xmax, ix_num)
+
+    seis=np.zeros((ix_num,tpnt))
+
+    for loop_ix in range(ix_num):
+        dtx=ix[loop_ix]*p
+        shiftr=(np.zeros((len(f),len(p)),dtype = complex)+(-1j))*2.0*np.pi
+
+        for l in range(len(f)):
+            for m in range(len(dtx)):
+                shiftr[l][m]=np.exp(shiftr[l][m]*f[l]*dtx[m])
+        trcf=f*np.sum(np.transpose(stpf)*shiftr,axis=1)
+        trcf[len(f)-1]=np.real(trcf[len(f)-1])
+        new_wave, _ = ifftrl(trcf,f,tpnt)
+        seis[loop_ix]=new_wave
+    seis=2*np.pi*seis/ix_num;
+
+    return seis, it, ix
