f9b67e57f911ce6e796abaa9ce8484ed83d40a7f
Pengcheng Chen <34759341+QuiltMan@users.noreply.github.com>
2022-03-13 11:30:03 +0800

    Update seis_tptran.py

--- seis_tptran.py
+++ seis_tptran.py
@@ -8,6 +8,7 @@ t_m=np.transpose(matlab_t['t'])
 t_mm=t_m[0]
 t_mm
 
+######PARAMETERS#########
 nx=200;
 nt=501;
 dx=10;
@@ -22,5 +23,14 @@ pmax=-pmin
 dp=.25*(pmax-pmin)/nx
 
 p_num=801
+######PARAMETERS#########
 
-stp, tau=tptran(np.transpose(shot),t_mm, xoff, pmin, pmax, dp, p_num)
+stp, tau, p=tptran(np.transpose(shot),t_mm, xoff, pmin, pmax, dp, p_num)
+
+######PARAMETERS#########
+xmin=xoff[0]
+xmax=xoff[len(xoff)-1]
+#dx=2*(xmax-xmin)/(len(p)-1);
+######PARAMETERS#########
+
+seis, it, ix=itptran(stp,tau, p, xmin, xmax, dx)
